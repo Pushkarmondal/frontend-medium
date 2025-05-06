bd86cb241c09ef5962adf8b9017e47b8a9803ca4
Pushkar Mondal <nishitm060@gmail.com>
2025-05-06 22:38:13 +0530

    Medium frontend init done

---
+++ src/components/Quote.tsx
@@ -0,0 +1,15 @@
+export const Quote = () => {
+      return (
+            <div className="bg-slate-200 h-screen flex items-center justify-center px-4">
+                  <div className="max-w-xl">
+                        <p className="text-xl md:text-2xl lg:text-3xl font-medium text-gray-900">
+                              “Innovation distinguishes between a leader and a follower.”
+                        </p>
+                        <div className="mt-4 text-m text-gray-700">
+                              <p className="font-bold">— Steve Jobs</p>
+                              <p className="text-gray-500">CEO, Apple Inc</p>
+                        </div>
+                  </div>
+            </div>
+      );
+};
